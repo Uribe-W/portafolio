66d4759cb289aa4f2c09e69209432b6af9ac9c8b
Uribe-W <wilsonuribe16@gmail.com>
2021-08-20 23:55:21 -0400

    se agregaron ajustes al responsive desing

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
 const navToggle = document.querySelector(".toggle");
 const navMenu = document.querySelector(".menu");
+const menu = document.querySelector(".link-nav");
 
 navToggle.addEventListener("click", ()=>{
     navMenu.classList.toggle("menu_visible");
 })
+
+menu.addEventListener("click", ()=>{
+    navMenu.classList.toggle("menu_visible");
+})
+
