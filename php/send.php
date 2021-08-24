f2fe44b69fbd1d9550b82df19ec6eca5269d1e93
Uribe-W <wilsonuribe16@gmail.com>
2021-08-23 20:57:13 -0400

    agregando envio de correo

--- php/send.php
+++ php/send.php
@@ -4,6 +4,17 @@ if (isset($_POST) && !empty($_POST)) {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $msj = $_POST["message"];  
+
+    ini_set( 'display_errors', 1 );
+    error_reporting( E_ALL );
+    $from = $email;
+    $to = "wilsonuribe16@gmail.com";
+    $subject = $name." TE HA EMBIADO UN MENSAJE DESDE TU PORTAFOLIO";
+    $message = $msj;
+    $headers = "From:" . $name;
+    mail($to,$subject,$message, $headers);
+    echo "The email message was sent.";
+
 }
 
 
